9f9a951e3c7bc390a81fdf9848fb81022e5d0f01
EXL <exlmotodev@gmail.com>
2021-09-26 17:29:56 +0700

    [drmhacker_magx]: Fix one-liners thanks to VINRARUS

--- drmhacker_magx/scripts/decrypt.sh
+++ drmhacker_magx/scripts/decrypt.sh
@@ -4,9 +4,17 @@
 . /etc/initservices/services/ezxenv.sh
 
 # One-liners for DCF file extensions
-find /usr/data_resource/picture -type f -name "*.dcf*" | xargs -n1 sh -c '../drmhacker_magx $0 `basename $0 .dcf`'
-find /usr/data_resource/sound/ringtones -type f -name "*.dcf*" | xargs -n1 sh -c '../drmhacker_magx $0 `basename $0 .dcf`'
+find /usr/data_resource/picture -type f -name "*.dcf*" | xargs -d '\n' -n1 sh -c '../drmhacker_magx "$0" `basename "$0" .dcf`'
+find /usr/data_resource/sound/ringtones -type f -name "*.dcf*" | xargs -d '\n' -n1 sh -c '../drmhacker_magx "$0" `basename "$0" .dcf`'
 
 # One-liners for DRM file extensions
-find /usr/data_resource/picture -type f -name "*.drm*" | xargs -n1 sh -c '../drmhacker_magx $0 `basename $0 | sed -E 's/\.drm//g'`'
-find /usr/data_resource/sound/ringtones -type f -name "*.drm*" | xargs -n1 sh -c '../drmhacker_magx $0 `basename $0 | sed -E 's/\.drm//g'`'
+find /usr/data_resource/picture -type f -name "*.drm*" | xargs -d '\n' -n1 sh -c '../drmhacker_magx "$0" `basename "$0" | sed -E 's/\.drm//g'`'
+find /usr/data_resource/sound/ringtones -type f -name "*.drm*" | xargs -d '\n' -n1 sh -c '../drmhacker_magx "$0" `basename "$0" | sed -E 's/\.drm//g'`'
+
+# Alternative one-liners by VINRARUS for DCF file extensions
+find /usr/data_resource/picture -type f -name "*.dcf*" | while read FILE; do F="${FILE%.*}"; ../drmhacker_magx "$FILE ${F##*/}"; done
+find /usr/data_resource/sound/ringtones -type f -name "*.dcf*" | while read FILE; do F="${FILE%.*}"; ../drmhacker_magx "$FILE ${F##*/}"; done
+
+# Alternative one-liners by VINRARUS for DRM file extensions
+find /usr/data_resource/picture -type f -name "*.drm*" | while read FILE; do F1="${FILE%.drm*}"; F2="${FILE##*.drm}"; ../drmhacker_magx "$FILE ${F1##*/}$F2"; done
+find /usr/data_resource/sound/ringtones -type f -name "*.drm*" | while read FILE; do F1="${FILE%.drm*}"; F2="${FILE##*.drm}"; ../drmhacker_magx "$FILE ${F1##*/}$F2"; done
